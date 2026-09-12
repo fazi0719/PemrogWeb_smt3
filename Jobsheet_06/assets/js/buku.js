516f6495b121973d6136c1a8e845928d9be36596
Alya Fazilatun Nisa <nisafazilatunalya@mail.com>
2026-09-12 17:55:56 +0700

    latihan 3

--- Jobsheet_06/assets/js/buku.js
+++ Jobsheet_06/assets/js/buku.js
@@ -1,6 +1,7 @@
 // Latihan 2 Jobsheet 6: Menggunakan Fungsi Generik
 document.addEventListener("DOMContentLoaded", function () {
-    const keysBuku = ["judul", "pengarang", "tahun", "stok"];
+    // latihan 3: menambahkan properti "kategori" ke daftar keysBuku
+    const keysBuku = ["judul", "pengarang", "tahun", "stok", "kategori"];
 
     // 1. Muat data pertama kali saat halaman dibuka
     muatDataTabel("../data/buku.json", keysBuku);
